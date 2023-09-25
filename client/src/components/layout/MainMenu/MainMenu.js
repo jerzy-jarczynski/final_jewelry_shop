ad000b7203fce6078424391f4f4f6ae56f38b268
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-09-25 21:44:54 +0200

    Add MainMenu scratch to Client

--- client/src/components/layout/MainMenu/MainMenu.js
+++ client/src/components/layout/MainMenu/MainMenu.js
@@ -1,9 +1,16 @@
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const MainMenu = () => {
+
   return (
-    <div>
-      Menu
-    </div>
+    <Navbar bg="primary" data-bs-theme="primary" className="rounded my-4 px-4">
+      <Navbar.Brand as={Link} to="/" className="text-white" style={{ textDecoration: "none" }}>
+        Home
+      </Navbar.Brand>
+    </Navbar>
   );
+
 };
 
 export default MainMenu;
