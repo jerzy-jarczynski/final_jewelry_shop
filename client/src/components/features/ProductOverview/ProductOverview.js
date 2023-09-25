094eab43edb7796213e6d93d11e352a465f1b0ea
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-09-25 20:08:05 +0200

    Add basic ProductOverview component structure and styles

--- client/src/components/features/ProductOverview/ProductOverview.js
+++ client/src/components/features/ProductOverview/ProductOverview.js
@@ -2,7 +2,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductById, loadProductsRequest } from "../../../redux/productsRedux";
 import { useEffect } from "react";
-import { Spinner } from "react-bootstrap";
+import { Row, Col, Button, Card, Spinner } from "react-bootstrap";
+import styles from "./ProductOverview.module.scss";
+import { IMGS_URL } from "../../../config";
 
 const ProductOverview = () => {
 
@@ -24,11 +26,79 @@ const ProductOverview = () => {
 
   console.log(data);
 
+  const productImageSrc = `${IMGS_URL}${data.photo}.png`;
+
   return (
-    <>
-      Hello World!
-    </>
+    // <Row>
+    //   <Col xs={12} md={10} lg={8} className="mx-auto">
+    //     <Card className="mb-4 p-3">
+    //       <Row>
+    //         <Col xs={12} md={6} lg={5}>
+    //           <div className={styles.imageContainer}>
+    //             <Card.Img variant="top" src={productImageSrc} />
+    //           </div>
+    //         </Col>
+    //         <Col xs={12} md={6} lg={7}>
+    //           <Card.Body>
+    //             <Card.Title>Title: {data.title}</Card.Title>
+    //             <Card.Text>Description: {data.description}</Card.Text>
+    //           </Card.Body>
+    //         </Col>
+    //       </Row>
+    //     </Card>
+    //   </Col>
+    // </Row>
+    <Row>
+      <Col>
+        <h2>
+          { data.title }
+        </h2>
+        <Card>
+          <Row>
+            <Col>
+              <div className={ styles.imageContainer }>
+                <Card.Img src={ productImageSrc } />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+          <Col>
+              <div className={ styles.imageContainer }>
+                <Card.Img src={ productImageSrc } />
+              </div>
+            </Col>
+            <Col>
+              <div className={ styles.imageContainer }>
+                <Card.Img src={ productImageSrc } />
+              </div>
+            </Col>
+            <Col>
+              <div className={ styles.imageContainer }>
+                <Card.Img src={ productImageSrc } />
+              </div>
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+      <Col>
+        <Row className={ styles.flexRow }>
+          <h2>
+            $ { data.price }
+          </h2>
+          <Card>
+            <Row>
+              <Col>
+                <Card.Body>
+                  <Card.Text>Description:<br/>{data.description}</Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        </Row>
+      </Col>
+    </Row>
   );
+
 
 }
 
