9fb6c2fca3f1f3d2d001a557838eaf854f30edc7
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-10-04 12:18:19 +0200

    Add module, controller && service for cart to backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { CartModule } from './cart/cart.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -29,6 +30,7 @@ import configuration from './config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    CartModule,
   ],
   controllers: [AppController],
   providers: [AppService],
