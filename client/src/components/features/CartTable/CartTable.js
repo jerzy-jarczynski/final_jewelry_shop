b34a30ce9b7d96b661d742bf19c9b7c3332e6fb1
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-10-05 11:47:27 +0200

    Handle removing from cart in the client

--- client/src/components/features/CartTable/CartTable.js
+++ client/src/components/features/CartTable/CartTable.js
@@ -1,51 +1,97 @@
-import { useSelector } from 'react-redux';
-import { Table, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Table, Button, Modal } from 'react-bootstrap';
 import { getProducts } from '../../../redux/productsRedux';
 import { returnImgSrc } from '../../../utils/renderImgSrc';
+import { deleteCartItemRequest, loadCartProductsRequest } from '../../../redux/cartRedux';
 
 const CartTable = ({ items }) => {
+    const dispatch = useDispatch();
     const allProducts = useSelector(state => getProducts(state));
+    const [showModal, setShowModal] = useState(false);
+    const [itemToDelete, setItemToDelete] = useState(null);
 
-    console.log(items);
+    const handleDelete = (item) => {
+        const productDetails = allProducts.find(product => product.id === item.productId);
+        setItemToDelete({ item, productDetails });
+        setShowModal(true);
+    };
+
+    const confirmDelete = () => {
+        if (itemToDelete && itemToDelete.item) {
+            dispatch(deleteCartItemRequest(itemToDelete.item.id))
+                .then(() => {
+                    dispatch(loadCartProductsRequest());
+                })
+                .catch(error => {
+                    console.error("Error deleting item:", error);
+                });
+        }
+        setShowModal(false);
+        setItemToDelete(null);
+    };
 
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Product Photo</th>
-                    <th>Amount</th>
-                    <th>Size</th>
-                    <th>Color</th>
-                    <th>Price</th>
-                    <th>Comment</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {items.map((item, index) => {
-                    const productDetails = allProducts.find(product => product.id === item.productId);
-                    return (
-                        <tr key={item.id}>
-                            <td>{index + 1}</td>
-                            <td>
-                              <img 
-                                src={returnImgSrc(productDetails.photo)} 
-                                alt="Product" 
-                                style={{ width: '100%', height: '100%', display: 'block', maxWidth: '150px' }} 
-                              />
-                            </td>
-                            <td>{item.amount}</td>
-                            <td>{item.size}</td>
-                            <td>{item.color}</td>
-                            <td>{productDetails.price}</td>
-                            <td>{item.comment}</td>
-                            <td><Button variant="danger">Delete</Button></td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </Table>
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Photo</th>
+                        <th>Product Title</th>
+                        <th>Amount</th>
+                        <th>Size</th>
+                        <th>Color</th>
+                        <th>Price</th>
+                        <th>Comment</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item, index) => {
+                        const productDetails = allProducts.find(product => product.id === item.productId);
+                        return (
+                            <tr key={item.id}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <img 
+                                        src={returnImgSrc(productDetails.photo)} 
+                                        alt="Product" 
+                                        style={{ width: '100%', height: '100%', display: 'block', maxWidth: '150px' }} 
+                                    />
+                                </td>
+                                <td>{productDetails.title}</td>
+                                <td>{item.amount}</td>
+                                <td>{item.size}</td>
+                                <td>{item.color}</td>
+                                <td>{productDetails.price}</td>
+                                <td>{item.comment}</td>
+                                <td>
+                                    <Button variant="danger" onClick={() => handleDelete(item)}>Delete</Button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Are you sure you want to delete {itemToDelete && itemToDelete.productDetails ? itemToDelete.productDetails.title : ''}?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                        Cancel
+                    </Button>
+                    <Button variant="danger" onClick={confirmDelete}>
+                        Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>   
+        </>
     );
 };
 
--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -5,9 +5,11 @@ import { API_URL } from "../config";
 const createActionName = (name) => `app/cart/${name}`;
 
 export const LOAD_CART = createActionName("LOAD_CART");
+export const DELETE_CART_ITEM = createActionName("DELETE_CART_ITEM");
 export const ERROR = createActionName("ERROR");
 
 export const loadCart = (payload) => ({ type: LOAD_CART, payload });
+export const deleteCartItem = (itemId) => ({ type: DELETE_CART_ITEM, payload: itemId });
 export const setError = (payload) => ({ type: ERROR, payload });
 
 // THUNKS
@@ -15,6 +17,15 @@ export const loadCartProductsRequest = () => async (dispatch) => {
   try {
     const res = await axios.get(`${API_URL}/cart`);
     dispatch(loadCart(res.data));
+  } catch (e) {
+    dispatch(setError(e.message));
+  }
+};
+
+export const deleteCartItemRequest = (itemId) => async (dispatch) => {
+  try {
+    await axios.delete(`${API_URL}/cart/${itemId}`);
+    dispatch(deleteCartItem(itemId));
   } catch (e) {
     dispatch(setError(e.message));
   }
@@ -33,6 +44,11 @@ const cartReducer = (statePart = [], action) => {
   switch (action.type) {
     case LOAD_CART:
       return { ...statePart, data: action.payload };
+    case DELETE_CART_ITEM:
+      return {
+        ...statePart,
+        data: statePart.data.filter(item => item.id !== action.payload)
+      };
     case ERROR:
       return { ...statePart, error: action.payload };
     default:
